3337a7db07cf2854bc824791450fac55f6cf62db
AmritKaur01 <124383345+AmritKaur01@users.noreply.github.com>
2023-02-04 10:10:34 +0000

    Program illustration of if-else

---
+++ IfElseConditions/ElseIfLAdder.java
@@ -0,0 +1,26 @@
+/*
+ * Program to learn basics of if-else-if ladder with examples
+ * For the example I will take a variable named wpm
+ * Comparing it with values in if-else-if condition, the program will print whether you are a pro or a beginner in typing,etc.
+ */
+
+public class ElseIfLAdder {
+    public static void main(String args[]) {
+        int wpm=32; //variable to store the value of words per minute
+        
+        //Based on the value of wpm it will print your typing speed status
+        if(wpm<15){
+            System.out.println("very slow");
+        }
+        else if(wpm<25) 
+            System.out.println("slow");
+        else if(wpm<35)
+            System.out.println("Average");
+        else if(wpm<50)
+            System.out.println("Good");
+        else if(wpm<60)
+            System.out.println("great");
+        else
+            System.out.println("Pro");
+    }
+}
---
+++ IfElseConditions/IfExample.java
@@ -0,0 +1,16 @@
+//Java program to get a basic understanding of if statements
+//If the condition in the if statement evaluates to true then then if block will be execueted
+//If the condition evaluates to false then that block of code will be ignored and execution will start after that
+
+public class IfExample{
+    public static void main(String args[])
+    {
+        String name = "Alice"; 
+
+        //checking the name
+        if(name.equals("Alice"))
+            System.out.println("Hello Alice");
+        
+        
+    }
+}
